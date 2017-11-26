c8b0b83de5704bb52d0f1c46854c1fc56353203f
Jason Axelson <jason.axelson@gmail.com>
2017-11-26 10:02:53 -1000

    Cleanup space age implementation

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -3,15 +3,15 @@ defmodule SpaceAge do
                 | :saturn | :uranus | :neptune
 
   @earth_year_seconds 31557600
-  @planets_year_length %{
-    earth: @earth_year_seconds,
-    mercury: 0.2408467 * @earth_year_seconds,
-    venus: 0.61519726 * @earth_year_seconds,
-    mars: 1.8808158 * @earth_year_seconds,
-    jupiter: 11.862615 * @earth_year_seconds,
-    saturn: 29.447498 * @earth_year_seconds,
-    uranus: 84.016846 * @earth_year_seconds,
-    neptune: 164.79132 * @earth_year_seconds,
+  @orbital_coefficients %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
   }
 
   @doc """
@@ -20,6 +20,7 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-    seconds / @planets_year_length[planet]
+    seconds_per_year = @orbital_coefficients[planet] * @earth_year_seconds
+    seconds / seconds_per_year
   end
 end
