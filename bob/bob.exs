c60738a59a41b06036e3988fb64d4397b905528c
Jason Axelson <jason.axelson@gmail.com>
2017-11-26 10:33:19 -1000

    First implementation of bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,15 @@
 defmodule Bob do
   def hey(input) do
-    cond do
-        true -> raise "Your implementation goes here"
+    last_char = String.last(input)
+    trimmed = String.trim(input)
+    has_letter = String.match?(input, ~r/\p{L}/u)
+    all_upper = String.match?(input, ~r/^(\p{Lu}|[^\p{L}])+$/u)
 
+    cond do
+      last_char == "?" -> "Sure."
+      trimmed == "" -> "Fine. Be that way!"
+      all_upper && has_letter -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end
