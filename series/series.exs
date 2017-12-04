232a265a49d5623c08a3091d886ddd31e1847ef8
Jason Axelson <jason.axelson@gmail.com>
2017-12-04 07:08:56 -1000

    Initial version of series.exs

--- series/series.exs
+++ series/series.exs
@@ -5,7 +5,16 @@ defmodule StringSeries do
   return an empty list.
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
-  def slices(_s, _size) do
+  def slices(_s, size) when size <= 0, do: []
+  def slices(string, size) do
+    {_, rest} = String.split_at(string, 1)
+    {slice, _} = String.split_at(string, size)
+
+    cond do
+      String.length(slice) < size -> []
+      String.length(rest) >= size -> [slice | slices(rest, size)]
+      true -> [slice]
+    end
   end
 end
 
