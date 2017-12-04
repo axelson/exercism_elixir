462898a9d65bd197f8bf950c34a76424ce0b9663
Jason Axelson <jason.axelson@gmail.com>
2017-12-04 07:23:47 -1000

    Traverse string fewer times and use more conditionals

--- series/series.exs
+++ series/series.exs
@@ -6,15 +6,16 @@ defmodule StringSeries do
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
   def slices(_s, size) when size <= 0, do: []
-  def slices(string, size) do
-    {_, rest} = String.split_at(string, 1)
-    {slice, _} = String.split_at(string, size)
-
-    cond do
-      String.length(slice) < size -> []
-      String.length(rest) >= size -> [slice | slices(rest, size)]
-      true -> [slice]
-    end
+  def slices(string, size) when is_binary(string),
+    do: slices(String.graphemes(string), size)
+  def slices(graphemes, size) when length(graphemes) < size, do: []
+  def slices(graphemes, size) do
+    {_, rest} = Enum.split(graphemes, 1)
+    slice =
+      graphemes
+      |> Enum.take(size)
+      |> Enum.join("")
+    [slice | slices(rest, size)]
   end
 end
 
