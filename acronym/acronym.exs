66eb95e066e36f4da53bcab869f0f393ace5c9a6
Jason Axelson <jason.axelson@gmail.com>
2017-12-03 21:57:02 -1000

    First attempt, didn't realize that MixedCase needs to be handled

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,5 +5,14 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split()
+    |> Enum.map(&first_character/1)
+    # Note: I don't like this here. Probably need to change my
+    # &first_character/1 implementation
+    |> Kernel.to_string()
+    |> String.upcase()
   end
+
+  defp first_character(<<char :: utf8, _rest :: binary>>), do: char
 end
