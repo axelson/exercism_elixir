f2650b49ad04591bb4e709e687630c5278ce1dfe
Jason Axelson <jason.axelson@gmail.com>
2017-12-22 22:38:04 -1000

    Sublist initial code

---
+++ sublist/lib/sublist.ex
@@ -0,0 +1,18 @@
+defmodule Sublist do
+  @moduledoc """
+  Documentation for Sublist.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sublist.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ sublist/test/sublist_test.exs
@@ -0,0 +1,8 @@
+defmodule SublistTest do
+  use ExUnit.Case
+  doctest Sublist
+
+  test "greets the world" do
+    assert Sublist.hello() == :world
+  end
+end
