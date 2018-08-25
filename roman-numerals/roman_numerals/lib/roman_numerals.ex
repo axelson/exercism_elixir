b751cc660c9a86f4f832071f170ed7ca3982054f
Jason Axelson <jason.axelson@gmail.com>
2018-08-25 10:23:19 -1000

    roman numerals: Generate code

--- roman-numerals/roman_numerals/lib/roman_numerals.ex
+++ roman-numerals/roman_numerals/lib/roman_numerals.ex
@@ -1,42 +1,35 @@
 defmodule Roman do
+  @values [
+    {"M", 1000},
+    {"CM", 900},
+    {"D", 500},
+    {"CD", 400},
+    {"C", 100},
+    {"XC", 90},
+    {"L", 50},
+    {"XL", 40},
+    {"X", 10},
+    {"IX", 9},
+    {"V", 5},
+    {"IV", 4},
+    {"I", 1}
+  ]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
     case numeral(number) do
-      {letters, 0} ->
-        letters
-
-      {letters, remaining} ->
-        letters <> numerals(remaining)
+      {letters, 0} -> letters
+      {letters, remaining} -> letters <> numerals(remaining)
     end
   end
 
-  @spec numeral(number) :: String.t() | {String.t(), integer}
-  def numeral(n) when n >= 1000, do: {"M", n - 1000}
-
-  def numeral(n) when n >= 900, do: {"CM", n - 900}
-
-  def numeral(n) when n >= 500, do: {"D", n - 500}
-
-  def numeral(n) when n >= 400, do: {"CD", n - 400}
-
-  def numeral(n) when n >= 100, do: {"C", n - 100}
-
-  def numeral(n) when n >= 90, do: {"XC", n - 90}
-
-  def numeral(n) when n >= 50, do: {"L", n - 50}
-
-  def numeral(n) when n >= 40, do: {"XL", n - 40}
-
-  def numeral(n) when n >= 10, do: {"X", n - 10}
-
-  def numeral(n) when n >= 9, do: {"IX", n - 9}
-
-  def numeral(n) when n >= 5, do: {"V", n - 5}
-
-  def numeral(n) when n >= 4, do: {"IV", n - 4}
-
-  def numeral(n) when n >= 1, do: {"I", n - 1}
+  @spec numeral(number) :: {String.t(), integer}
+  for {letter, value} <- @values do
+    def numeral(n) when n >= unquote(value) do
+      {unquote(letter), n - unquote(value)}
+    end
+  end
 end
