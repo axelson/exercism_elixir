6b969d0512b6a20adda9a03e4149f129c5f7f825
Jason Axelson <jason.axelson@gmail.com>
2023-06-17 15:03:20 -1000

    finish guessing-game

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,5 +1,12 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
-  end
+  def compare(_secret_number), do: "Make a guess"
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+  
+  def compare(secret_number, secret_number), do: "Correct"
+
+  def compare(secret_number, guess) when guess == secret_number + 1, do: "So close"
+  def compare(secret_number, guess) when guess == secret_number - 1, do: "So close"
+
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
 end
