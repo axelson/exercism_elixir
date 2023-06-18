261e018816f5ad64b227fae6a559781b68f1d1cc
Jason Axelson <jason.axelson@gmail.com>
2023-06-17 14:35:01 -1000

    finish log-level

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,36 @@
 defmodule LogLevel do
+  @log_codes %{
+    0 => {:trace, false},
+    1 => {:debug, true},
+    2 => {:info, true},
+    3 => {:warning, true},
+    4 => {:error, true},
+    5 => {:fatal, false}
+  }
+  @unknown :unknown
+
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    case Map.get(@log_codes, level) do
+      {label, legacy_supported} ->
+        if legacy? && !legacy_supported do
+          @unknown
+        else
+          label
+        end
+
+      nil ->
+        @unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+
+    cond do
+      label in [:error, :fatal] -> :ops
+      label == :unknown && legacy? -> :dev1
+      label == :unknown -> :dev2
+      true -> false
+    end
   end
 end
